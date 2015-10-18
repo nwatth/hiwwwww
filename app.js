b10539d7a27932426920b27281f3a1754ec2ddc0
Nantiwat Thongseng <nwatth@gmail.com>
2015-10-18 22:03:13 +0700

    Fix app started

--- app.js
+++ app.js
@@ -26,10 +26,11 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.send('error', {
       message: err.message,
       error: err
     });
+    return;
   });
 }
 
@@ -37,10 +38,11 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.send('error', {
     message: err.message,
     error: {}
   });
+  return;
 });
 
 
